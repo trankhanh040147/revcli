277561865aec65444da0a26bf4689a8a07ff54d1
trankhanh040147 <trankhanh040147@gmail.com>
2026-01-07 18:04:07 +0700

    Revert "fix: version build mismatch"
    This reverts commit 8a4daa60279887406ce9fb565f75c1e336a6e275.

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -387,11 +387,9 @@ func (a *appModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	// Update Available
 	case pubsub.UpdateAvailableMsg:
 		// Show update notification in status bar
-		statusMsg := fmt.Sprintf("RevCLI update available: v%s → v%s.", msg.CurrentVersion, msg.LatestVersion)
+		statusMsg := fmt.Sprintf("Crush update available: v%s → v%s.", msg.CurrentVersion, msg.LatestVersion)
 		if msg.IsDevelopment {
-			// todo: handle get latest version
-			// statusMsg = fmt.Sprintf("This is a development version of RevCLI. The latest version is v%s.", msg.LatestVersion)
-			statusMsg = fmt.Sprintf("This is a development version of RevCLI. The latest version is v%s.", msg.CurrentVersion)
+			statusMsg = fmt.Sprintf("This is a development version of Crush. The latest version is v%s.", msg.LatestVersion)
 		}
 		s, statusCmd := a.status.Update(util.InfoMsg{
 			Type: util.InfoTypeUpdate,
